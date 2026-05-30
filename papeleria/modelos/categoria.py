ee553db111eca40d064c3f927f4d394901a358db
Joel Cornelio <joelosorio2006@gmail.com>
2026-05-29 19:55:52 -0600

    Modificar atributos de la categoria

--- papeleria/modelos/categoria.py
+++ papeleria/modelos/categoria.py
@@ -8,8 +8,8 @@ class Categoria:
     """Representa la clasificación de los productos."""
 
     def __init__(self, id_categoria, nombre_categoria):
-        self.id_categoria = id_categoria
-        self.nombre_categoria = nombre_categoria
+        self.id = id_categoria
+        self.nombre = nombre_categoria
 
     def __str__(self):
-        return self.nombre_categoria
+        return self.nombre
