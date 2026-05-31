a7f4cd07f100f272a4c6d1f4ca275310d03446b7
Joel Cornelio <joelosorio2006@gmail.com>
2026-05-30 18:21:42 -0600

    Actualizar el archivo __init__ del modulo datos

--- papeleria/datos/__init__.py
+++ papeleria/datos/__init__.py
@@ -1,2 +1,6 @@
 # datos/__init__.py
-from .almacen import Almacen
+from .categorias_repositorio import CategoriaRepositorio
+from .cliente_repositorio import ClienteRepositorio
+from .producto_repositorio import ProductoRepositorio
+from .marca_repositorio import MarcaRepositorio
+from .proveedor_repositorio import ProveedorRepositorio
