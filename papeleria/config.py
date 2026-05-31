a17cc5d955e59618de4fcff7faf50f112125d035
Joel Cornelio <joelosorio2006@gmail.com>
2026-05-30 22:43:34 -0600

    Actualizar el script de config

--- papeleria/config.py
+++ papeleria/config.py
@@ -10,5 +10,6 @@ def obtener_config_db():
         'host': os.environ.get('DB_HOST'),
         'user': os.environ.get('DB_USER'),
         'password': os.environ.get('DB_PASSWORD'),
-        'database': os.environ.get('DB_NAME') 
+        'database': os.environ.get('DB_NAME'),
+        'port': int(os.environ.get('DB_PORT'), 3306)  # Puerto por defecto de MySQL
     }
