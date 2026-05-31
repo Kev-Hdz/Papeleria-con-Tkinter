1ea6897a745a811edc3a3652b4d190b581a3855f
Joel Cornelio <joelosorio2006@gmail.com>
2026-05-31 17:07:00 -0600

    Modificar el orden de importacion

--- papeleria/datos/__init__.py
+++ papeleria/datos/__init__.py
@@ -1,6 +1,6 @@
 # datos/__init__.py
 from .categorias_repositorio import CategoriaRepositorio
-from .cliente_repositorio import ClienteRepositorio
+from .contacto_repositorio import ContactoRepositorio
 from .producto_repositorio import ProductoRepositorio
 from .marca_repositorio import MarcaRepositorio
-from .proveedor_repositorio import ProveedorRepositorio
+
