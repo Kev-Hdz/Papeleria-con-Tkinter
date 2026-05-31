579e92b9fa9fb5dc08c61e7cda1d3626c3b08f19
Joel Cornelio <joelosorio2006@gmail.com>
2026-05-30 18:13:31 -0600

    Agregar script para obtener las variables de entorno

---
+++ papeleria/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+def obtener_config_db():
+    """Carga las variables del .env y devuelve el diccionario de conexión."""
+    # Carga las variables de entorno desde el archivo .env
+    load_dotenv()
+    
+    return {
+        'host': os.environ.get('DB_HOST'),
+        'user': os.environ.get('DB_USER'),
+        'password': os.environ.get('DB_PASSWORD'),
+        'database': os.environ.get('DB_NAME') 
+    }
