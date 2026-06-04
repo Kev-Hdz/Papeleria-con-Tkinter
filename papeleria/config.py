0847ed106e14cb956789970304b03038f060c3a1
Joel Cornelio <joelosorio2006@gmail.com>
2026-06-04 15:45:33 -0600

    Eliminar error al indicar el puerto

--- papeleria/config.py
+++ papeleria/config.py
@@ -10,6 +10,5 @@ def obtener_config_db():
         'host': os.environ.get('DB_HOST'),
         'user': os.environ.get('DB_USER'),
         'password': os.environ.get('DB_PASSWORD'),
-        'database': os.environ.get('DB_NAME'),
-        'port': int(os.environ.get('DB_PORT'), 3306)  # Puerto por defecto de MySQL
+        'database': os.environ.get('DB_NAME')# Puerto por defecto de MySQL
     }
